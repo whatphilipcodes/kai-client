b7a05322045c2f62b847c673621aa244b4291f12
whatphilipcodes <gerdes.philip@gmail.com>
2026-08-12 00:52:23 +0200

    fix: missing logger init

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import asyncio
+
+from kai_shared.io.node import PipelineNode
+from kai_shared.utils.logger import setup_logging
+
 from src.kai_client.config_client import settings_client
 
 
-def main():
-    print(f"Hello from {settings_client.shared.network.node_id}")
+async def main() -> None:
+    setup_logging()
+    node = PipelineNode(settings_client.shared)
+    await node.run()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
