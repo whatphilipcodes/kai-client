779200a574974557d4b021db13f7daa9d920f1df
whatphilipcodes <gerdes.philip@gmail.com>
2026-08-12 02:14:37 +0200

    chore: up shared

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import asyncio
+import uvloop
 
 from kai_shared.io.node import PipelineNode
 from kai_shared.utils.logger import setup_logging
@@ -13,4 +13,4 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    uvloop.run(main())
